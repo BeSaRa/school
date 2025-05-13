0e40d20a66406e28ea30c98b37eac43ee687560e
aalshahin <aalshahin@eblacorp.com>
2025-05-14 02:21:58 +0400

    feat(markdown): add KaTeX support for rendering mathematical expressions
    Integrate KaTeX into the application to enable rendering of mathematical expressions in markdown content. This includes adding KaTeX dependencies, updating styles, and creating a custom markdown service to handle KaTeX tokenization.

--- src/components/chat-message/chat-message.component.ts
+++ src/components/chat-message/chat-message.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input } from "@angular/core";
+import { Component, inject, Input } from "@angular/core";
 import { Message } from "@/models/message";
 import { CommonModule } from "@angular/common";
-import { MarkdownModule } from "ngx-markdown";
+import { MarkdownModule, MARKED_OPTIONS } from "ngx-markdown";
+import { MarkdownService } from "../../services/markdown.service";
 
 @Component({
   selector: "app-chat-message",
   standalone: true,
   imports: [CommonModule, MarkdownModule],
   templateUrl: "./chat-message.component.html",
+  // providers: [
+  //   {
+  //     provide: MARKED_OPTIONS,
+  //     useFactory: (markdownService: MarkdownService) =>
+  //       markdownService.getMarkedOptions(),
+  //     deps: [MarkdownService],
+  //   },
+  // ],
 })
 export class ChatMessageComponent {
   @Input() message!: Message;
+  markdownService = inject(MarkdownService);
 }
