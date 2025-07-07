253d3f05fac2e21d0014c77625e1dcb19ad0fd91
aalshahin <aalshahin@eblacorp.com>
2025-07-08 02:44:24 +0400

    feat(loading): add skip loading context for http requests
    Add SKIP_LOADING token to allow skipping loading indicator for specific requests
    Update conversation service to skip loading for getConversations calls

--- src/interceptors/loading.interceptor.ts
+++ src/interceptors/loading.interceptor.ts
@@ -2,9 +2,15 @@ import { HttpInterceptorFn } from "@angular/common/http";
 import { inject } from "@angular/core";
 import { LoadingService } from "@/services/loading.service";
 import { finalize } from "rxjs";
+import { SKIP_LOADING } from "../tokens/loading-context.token";
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const loadingService = inject(LoadingService);
+
+  const skip = req.context.get(SKIP_LOADING);
+  if (skip) {
+    return next(req);
+  }
 
   loadingService.startLoading();
 
